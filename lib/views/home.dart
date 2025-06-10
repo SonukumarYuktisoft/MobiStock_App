66697341b2366e769225de8abe16a0b8623cdd85
SonukumarYuktisoft <sonukumar@yuktisoft.in>
2025-06-10 13:16:57 +0530

    intial commit

---
+++ lib/views/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Welcome to the Home Page!'),
+            ElevatedButton(
+              onPressed: () {
+                // Action when button is pressed
+              },
+              child: const Text('Press Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
